4505113e3aa7d15a8cc9042c55593dafcd478752
Saarthak Chaturvedi <saarthakchats05@gmail.com>
2019-07-25 12:54:22 +0530

    Landing

--- jpisforum/urls.py
+++ jpisforum/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', posts.views.home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('posts/', include('posts.urls')),
-    # path('posts/', include('posts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
